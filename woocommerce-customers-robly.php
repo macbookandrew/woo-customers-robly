c80ac21b8756b3c562a3398bcceb4b3fd6befcfb
Andrew Minion <andrew@andrewrminion.com>
2016-01-04 13:32:09 -0500

    bump version number

--- woocommerce-customers-robly.php
+++ woocommerce-customers-robly.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: WooCommerce Customers to Robly
- * Version: 1.1
+ * Version: 1.1.1
  * Description: Adds WooCommerce customers to Robly using their API
  * Author: AndrewRMinion Design
  * Author URI: https://andrewrminion.com
